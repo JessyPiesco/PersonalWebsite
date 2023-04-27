e474f1103f33d907f74c17dbc7575a535cdfac6e
Jessy <japarker0421@gmail.com>
2023-04-26 18:31:27 -0600

    fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,21 +2,30 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div id="footer" class="d-flex justify-content-center align-items-center">
-    <span class="pr-2">Connect with me:</span>
-    <span class="ml-3">
-      <a href="https://www.linkedin.com/in/jessypiesco/" class="text-light">•LinkedIn</a>
-    </span>
-    <span class="ml-3">
-      <a href="https://github.com/JessyPiesco" class="text-light">•GitHub</a>
-    </span>
-    <span class="ml-3">
-      <a href="mailto:japiesco@outlook.com" class="text-light">•Contact Me</a>
-    </span>
-
-  </div>
-
+    <div id="footer" className="d-flex justify-content-center align-items-center flex-wrap">
+      <span className="pr-2">Connect</span>
+      <span className="ml-3 d-none d-md-block">
+        <a href="https://www.linkedin.com/in/jessypiesco/" className="text-light">•LinkedIn</a>
+      </span>
+      <span className="ml-3 d-none d-md-block">
+        <a href="https://github.com/JessyPiesco" className="text-light">•GitHub</a>
+      </span>
+      <span className="ml-3 d-none d-md-block">
+        <a href="mailto:japiesco@outlook.com" className="text-light">•Message</a>
+      </span>
+      <span className="ml-3 d-md-none">
+        <a href="https://www.linkedin.com/in/jessypiesco/" className="text-light">LinkedIn</a>
+      </span>
+      <span className="ml-3 d-md-none">
+        <a href="https://github.com/JessyPiesco" className="text-light">GitHub</a>
+      </span>
+      <span className="ml-3 d-md-none">
+        <a href="mailto:japiesco@outlook.com" className="text-light">Message</a>
+      </span>
+    </div>
   );
 };
+
+
 
 export default Footer;
