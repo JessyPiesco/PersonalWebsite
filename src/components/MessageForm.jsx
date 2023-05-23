931e335a26b1c53352648f94fff292979e1a40a5
Jessy <japarker0421@gmail.com>
2023-05-23 11:55:25 -0600

    got languages looking better for small screens and updated look of the message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -40,16 +40,23 @@ const MessageForm = () => {
       from_user_email: email,
     };
 
-    emailjs.send("service_wv68rfn", "template_6eu14u8", templateParams,"p11l9eyN6C8GVyUKL" ).then(
-      (response) => {
-        console.log("Email sent successfully:", response);
-        // Add any success message or further actions you want to perform
-      },
-      (error) => {
-        console.error("Email sending failed:", error);
-        // Add any error handling or display an error message
-      }
-    );
+    emailjs
+      .send(
+        "service_wv68rfn",
+        "template_6eu14u8",
+        templateParams,
+        "p11l9eyN6C8GVyUKL"
+      )
+      .then(
+        (response) => {
+          console.log("Email sent successfully:", response);
+          // Add any success message or further actions you want to perform
+        },
+        (error) => {
+          console.error("Email sending failed:", error);
+          // Add any error handling or display an error message
+        }
+      );
 
     // Clear form fields after submission
     setName("");
@@ -58,80 +65,78 @@ const MessageForm = () => {
   };
 
   return (
-    <div id= "messageForm"className="container-fluid">
-    <div className="row justify-content-center">
-      <div className="col-lg-2 col-12 order-md-first sides-container pb-1 p-0">
-        <LeftSide />
-      </div>
-      <div className="col-lg-8 col-md-10 pt-2">
-        <div id="messageBox" className="border p-4">
-          <h2 className="text-center">Message Form</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="selectOption" className="form-label">
-              Select an option:
-            </label>
-            <select
-              className="form-select"
-              id="selectOption"
-              value={selectedOption}
-              onChange={handleOptionChange}
-            >
-              {options.map((option) => (
-                <option key={option} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
+    <div id="messageForm" className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-lg-2 col-12 order-md-first sides-container pb-1 p-0">
+          <LeftSide />
+        </div>
+        <div className="col-lg-8 col-md-10 pt-2">
+          <div id="messageBox" className="border p-4">
+            <h2 className="text-center">Send me a message!</h2>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="selectOption" className="form-label">
+                  Select an option:
+                </label>
+                <select
+                  className="form-select"
+                  id="selectOption"
+                  value={selectedOption}
+                  onChange={handleOptionChange}
+                >
+                  {options.map((option) => (
+                    <option key={option} value={option}>
+                      {option}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="inputName" className="form-label">
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputName"
+                  value={name}
+                  onChange={handleNameChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="inputEmail" className="form-label">
+                  Email:
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="inputEmail"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="inputMessage" className="form-label">
+                  Message:
+                </label>
+                <textarea
+                  className="form-control textarea-custom"
+                  id="inputMessage"
+                  value={message}
+                  onChange={handleMessageChange}
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </form>
           </div>
-          <div className="mb-3">
-            <label htmlFor="inputName" className="form-label">
-              Name:
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputName"
-              value={name}
-              onChange={handleNameChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="inputEmail" className="form-label">
-              Email:
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="inputEmail"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="inputMessage" className="form-label">
-              Message:
-            </label>
-            <textarea
-              className="form-control"
-              id="inputMessage"
-              value={message}
-              onChange={handleMessageChange}
-            ></textarea>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        </div>
+        <div className="col-lg-2 col-12 order-md-last sides-container pb-1 p-0">
+          <RightSide />
+        </div>
       </div>
     </div>
-    <div className="col-lg-2 col-12 order-md-last sides-container pb-1 p-0">
-      <RightSide />
-    </div>
-  </div>
-
-</div>
-
   );
 };
 
