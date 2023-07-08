8148038bfcd0754355809dde0f2f42e030d448a6
Jessy <japarker0421@gmail.com>
2023-07-07 18:26:55 -0600

    animated brand

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-      <h1 className="navbar-brand">Your Developer, Jessy Piesco!</h1>
+const brandRef = useRef(null);
+
+useEffect(() => {
+  const brandElement = brandRef.current;
+  const text = brandElement.textContent;
+  const speed = 100;
+
+  let i = 0;
+  brandElement.textContent = "";
+
+  const typeWriter = () => {
+    if (i < text.length) {
+      brandElement.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  };
+
+  typeWriter();
+}, []);
+
+
+return (
+  <div className="navbar">
+    <h1 className="navbar-brand">
+      <span className="typewriter" ref={brandRef}>
+        Your Developer, Jessy Piesco
+      </span>
+    </h1>
+
       <div className="navbar-nav flex-row">
       <NavLink
           className="nav-link"
